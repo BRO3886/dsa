9245e9e2d5eb0514a89b8210e7d9106fcdc49b43
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-09 15:52:11 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.py
+++ Palindrome String - GFG/palindrome-string.py
@@ -1,25 +1,21 @@
 #User function Template for python3
 class Solution:
 	def isPlaindrome(self, S):
-	    """
-        check length
-        odd => ignore element at n/2 then
-        even => reverse second half and compare with first half 
-        """
-        n = len(S)
-        
-        first_half = S[:n//2]
-        sec_half = S[n//2:]
-        if n%2 != 0:
-            sec_half = S[n//2+1:]
-        
-        sec_half = sec_half[::-1]
-        
-        # print(first_half,sec_half)
-        
-        if first_half == sec_half:
-            return 1
-        return 0
+	   # check length
+	   # odd => ignore element at n/2 then
+	   # even => reverse second half and compare with first half 
+	   n = len(S)
+	   first_half = S[:n//2]
+	   sec_half = S[n//2:]
+	   if n%2 != 0:
+	       sec_half = S[n//2+1:]
+	   
+	   sec_half = sec_half[::-1]
+	   # print(first_half,sec_half)
+	   
+	   if first_half == sec_half:
+	       return 1
+	   return 0
             
 #{ 
 #  Driver Code Starts
