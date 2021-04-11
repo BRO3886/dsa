4aefd689d468ad6a3ca4182c74ca6301eff6f670
Siddhartha Varma <siddverma1999@gmail.com>
2021-04-11 18:01:17 +0530

    Time: 44 ms (44.81%), Space: 14.1 MB (80.03%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -1,31 +1,42 @@
 class Solution:
-    
-    def reverse(self,nums:List[int],start:int)->None:
+    def reverse(self,arr: List[int], start: int) -> None:
         i = start
-        j = len(nums) -1;
+        j = len(arr) - 1
         while(i<j):
-            # swap nums[i] and nums[j]
-            nums[i],nums[j]=nums[j],nums[i]
+            arr[i],arr[j] = arr[j],arr[i]
             i+=1
             j-=1
-    
     def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # case where it's strictly decreasing order, return increasing
+        t = sorted(nums,reverse=True)
+        if t == nums:
+            return nums.sort()
+        # len of array
         n = len(nums)
         i = n-2
-        
-        # find decreasing number pos
-        while (i >= 0 and nums[i+1] <= nums[i]):
+        # 1 3 5 1 0
+        # keep checking till you find left element arr[i-1] smaller than right arr[i]
+        while(i >= 0 and nums[i] >= nums[i+1]):
             i-=1
-        
-        if i >=0:
-            j = n -1
-            # find number just larger than nums[i]
-            while(j>=0 and nums[j] <= nums[i]):
-                j-=1            
-                
-            # swap nums[i] and nums[j]
-            nums[i],nums[j]=nums[j],nums[i]
-            
+        # check if i was actually bigger
+        if i>=0:
+            # another iterator
+            j = n - 1
+            # find next higher number than arr[i]
+            while(j >= 0 and nums[j] <= nums[i]):
+                j-=1
+            # swap i and j
+            nums[i],nums[j] = nums[j],nums[i]
         # reverse sublist
         self.reverse(nums,i+1)
+                
+            
+            
+            
+        
+        
+        
         
