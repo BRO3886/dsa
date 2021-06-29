05fc120fc1d6f471efa060535e0fe431a3e9bbb9
Siddhartha Varma <siddverma1999@gmail.com>
2021-06-29 10:10:48 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.py
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.py
@@ -2,6 +2,15 @@
 
 class Solution:
     def getMinDiff(self, arr, n, k):
+        """
+        1. sort the array
+        2. take the max difference from starting and ending
+        3. iterate through the array
+            3.1. check if next element is more than k (to handle -ve)
+            3.2. take currMin as the min of next+k and start+k
+            3.3. take currMax as the max of curr+k,last-k
+            3.4. set ans as min of ans and diff of currMax and currMin
+        """
         arr.sort()
         ans = arr[n-1]-arr[0]
         for i in range(n-1):
