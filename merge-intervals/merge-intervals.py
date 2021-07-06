c069cf711d79d27ed0ea079563b81cdbc72e33c2
Siddhartha Varma <siddverma1999@gmail.com>
2021-07-06 23:45:12 +0530

    Time: 76 ms (97.60%), Space: 16.2 MB (53.76%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,19 +1,15 @@
 class Solution:
-    
-    def is_overlap(self,a,b):
-        return a[0] <= b[1] and b[0] <= a[1]
-    
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) == 0:
+        n = len(intervals)
+        if n == 0:
             return []
-        intervals = sorted(intervals, key=lambda i:i[0])
-        
+        intervals = sorted(intervals, key = lambda i:i[0])
         res = [intervals[0]]
-        
         for i in intervals[1:]:
             if res[-1][1] >= i[0]:
-                res[-1][1] = max(res[-1][1],i[1])
+                res[-1][1] = max(i[1],res[-1][1])
             else:
                 res.append(i)
                 
         return res
+            
