305c170f59730bb94fb416013d44878944f8f154
Siddhartha Varma <siddverma1999@gmail.com>
2021-08-21 17:52:17 +0530

    Time: 0 ms (100.00%), Space: 36.5 MB (36.73%) - LeetHub

--- number-of-good-pairs/number-of-good-pairs.java
+++ number-of-good-pairs/number-of-good-pairs.java
@@ -1,14 +1,23 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for(int num: nums){
+            hm.put(num, hm.getOrDefault(num,0)+1);
+            // subtracting count value because in
+            // [1,2,3,1,1,3] 
+            // If there are two occurrence of element 3,
+            // we can form 1 pair
+            // but if there is another occurence, we have total 
+            // 3 pairs (2 new, 1 old)
+            // since the prev one is already counted we subtract 1
+            count+=hm.get(num)-1;
         }
         
         return count;
+        
+        
     }
 }
