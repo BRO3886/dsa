b56ae5f27723b85c79eb39643a6f3d54f50863ae
Siddhartha Varma <siddverma1999@gmail.com>
2021-08-27 00:44:47 +0530

    Time: 1 ms (40.03%), Space: 39.2 MB (5.18%) - LeetHub

--- determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -2,20 +2,20 @@ class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
         
-        boolean[] c = new boolean[4];
+        boolean[] checksum = new boolean[4];
         
-        Arrays.fill(c, true);
+        Arrays.fill(checksum, true);
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j<n; j++){
-                if(mat[i][j]!=target[i][j]) c[0]=false;
-                if(mat[i][j]!=target[n-j-1][i]) c[1]=false;
-                if(mat[i][j]!=target[n-i-1][n-j-1]) c[2]=false;
-                if(mat[i][j]!=target[j][n-i-1]) c[3]=false;
+                if(mat[i][j] != target[i][j]) checksum[0] = false;
+                if(mat[i][j] != target[n-1-j][i]) checksum[1] = false;
+                if(mat[i][j] != target[n-1-i][n-1-j]) checksum[2] = false;
+                if(mat[i][j] != target[j][n-1-i]) checksum[3] = false;
             }
         }
         
-        return c[0] || c[1] || c[2] || c[3];
+        return checksum[0] || checksum[1] || checksum[2] || checksum[3];
         
     }
 }
