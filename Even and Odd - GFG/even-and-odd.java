8fc8639f789ddb44760c844a6c6c27e549d2bf15
Siddhartha Varma <siddverma1999@gmail.com>
2021-11-30 09:49:11 +0530

    Added solution - LeetHub

---
+++ Even and Odd - GFG/even-and-odd.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] arr = new int[N];
+            
+            for(int i=0; i<N; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            ob.reArrange(arr,N);
+            
+            System.out.println(check(arr,N));
+        }
+    }
+    static int check(int arr[], int n)
+    {
+        int flag = 1;
+        int c=0, d=0;
+        for(int i=0; i<n; i++)
+        {
+            if(i%2==0)
+            {
+                if(arr[i]%2==1)
+                {
+                    flag = 0;
+                    break;
+                }
+                else
+                    c++;
+            }
+            else
+            {
+                if(arr[i]%2==0)
+                {
+                    flag = 0;
+                    break;
+                }
+                else
+                    d++;
+            }
+        }
+        if(c!=d)
+            flag = 0;
+                
+        return flag;
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static void reArrange(int[] arr, int n) {
+        int oddIdx = 1;
+        int evenIdx = 0;
+        while(true){
+            while(evenIdx < n && arr[evenIdx]%2 == 0){
+                evenIdx += 2;
+            }
+            while(oddIdx < n && arr[oddIdx]%2 == 1) {
+                oddIdx +=2;
+            }
+            
+            if(evenIdx < n && oddIdx < n) {
+                int temp = arr[evenIdx];
+                arr[evenIdx] = arr[oddIdx];
+                arr[oddIdx] = temp;
+            } else break;
+        }
+        
+    }
+};
