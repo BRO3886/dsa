5e06af58c77fd77ca573e77690b514fd31a3d124
Siddhartha Varma <siddverma1999@gmail.com>
2022-02-20 23:05:57 +0530

    Time: 61 ms (5.12%), Space: 71.9 MB (30.91%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean containsDuplicateDict(int[] nums) {
+    public boolean containsDuplicateS(int[] nums) {
         /*
         approaches:
         1. make a dict
@@ -10,6 +10,21 @@ class Solution {
     }
     
     public boolean containsDuplicate(int[] nums) {
+        // approach 1
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int n: nums) {
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 1) return true;
+        }
+        
+        return false;
+    }
+    
+    private boolean containsDuplicateSet(int[] nums) {
         // approach 2
         Set<Integer> hs = new HashSet<>();
         for(int n: nums) {
