40f31d1fb7d376bfe20a7f112f4cd8aa047ed776
Siddhartha Varma <siddverma1999@gmail.com>
2022-03-01 23:19:30 +0530

    Time: 2 ms (82.30%), Space: 47.8 MB (41.65%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr = new int[n+1];
-        
-        for(int i=0; i <= n; i++){
-            arr[i] = findOnes(i);
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int offset = 1;
+        for(int i = 1; i <= n; i++) {
+            if(offset*2 == i) {
+                offset = i;
+            }
+            dp[i] = 1 + dp[i-offset];
         }
         
-        return arr;
+        return dp;
     }
-
-    private int findOnes(int n) {
-        int count = 0;
-        while(n != 0) {
-            n = n&(n-1);
-            count++;
-        }
-        
-        return count;
-    } 
 }
